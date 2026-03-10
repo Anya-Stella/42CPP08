1d3d0363f3e64cae2ed796e579a6ab461d56d7ca
Tsubasa Ishihara <tishihar@c6r2s8.42tokyo.jp>
2026-03-10 16:59:32 +0900

    fin ex02

--- ex02/include/MutantStack.hpp
+++ ex02/include/MutantStack.hpp
@@ -1,2 +1,37 @@
 #pragma once
+#include <stack>
+#include <deque>
+
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+public:
+	typedef std::stack<T, Container>					stack_type;
+	typedef typename Container::iterator				iterator;
+	typedef typename Container::const_iterator			const_iterator;
+	typedef typename Container::reverse_iterator		reverse_iterator;
+	typedef typename Container::const_reverse_iterator	const_reverse_iterator;
+
+	MutantStack() : stack_type() {}
+	MutantStack(const MutantStack& other) : stack_type(other) {}
+	MutantStack& operator=(const MutantStack& rhs)
+	{
+		stack_type::operator=(rhs);
+		return *this;
+	}
+	~MutantStack() {}
+
+	iterator	begin() { return this->c.begin(); }
+	iterator	end() { return this->c.end(); }
+
+	const_iterator	begin() const { return this->c.begin(); }
+	const_iterator	end() const { return this->c.end(); }
+
+	reverse_iterator	rbegin() { return this->c.rbegin(); }
+	reverse_iterator	rend() { return this->c.rend(); }
+
+	const_reverse_iterator	rbegin() const { return this->c.rbegin(); }
+	const_reverse_iterator	rend() const { return this->c.rend(); }
+
+};
 
